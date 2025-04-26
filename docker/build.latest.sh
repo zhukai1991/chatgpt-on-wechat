45c5f182bb3c44aeb6af9616737ed142a93bf7fc
zhukai@sunline.cn <1664940968@qq.com>
2025-04-26 17:15:27 +0800

    Update build.latest.sh

--- docker/build.latest.sh
+++ docker/build.latest.sh
@@ -3,6 +3,6 @@
 unset KUBECONFIG
 
 cd .. && docker build -f docker/Dockerfile.latest \
-             -t zhayujie/chatgpt-on-wechat .
+             -t zhukai1991/chatgpt-on-wechat .
 
-docker tag zhayujie/chatgpt-on-wechat zhayujie/chatgpt-on-wechat:$(date +%y%m%d)
+docker tag zhukai1991/chatgpt-on-wechat zack2025/chatgpt-on-wechat:$(date +%y%m%d)
